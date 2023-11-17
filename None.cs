e0db4928104285594e5720eeb3336f62516a3491
DavveDP <davve146@gmail.com>
2023-11-17 16:41:23 +0100

    Added using system and namespace brackets

--- None.cs
+++ None.cs
@@ -1,43 +1,46 @@
-namespace Monads;
+using System;
 
-public sealed class None<T> : Option<T>
+namespace FPUtils
 {
-    internal None() : base(default)
+    public sealed class None<T> : Option<T>
     {
-    }
+        internal None() : base(default)
+        {
+        }
 
-    public override Option<V> Map<V>(Func<T, V> transformation)
-    {
-        return new None<V>();
-    }
+        public override Option<V> Map<V>(Func<T, V> transformation)
+        {
+            return new None<V>();
+        }
 
-    public override Option<V> FlatMap<V>(Func<T, Option<V>> transformation)
-    {
-        return new None<V>();
-    }
+        public override Option<V> FlatMap<V>(Func<T, Option<V>> transformation)
+        {
+            return new None<V>();
+        }
 
-    public override Option<T> Filter(Func<T, bool> predicate)
-    {
-        return this;
-    }
+        public override Option<T> Filter(Func<T, bool> predicate)
+        {
+            return this;
+        }
 
-    public override void IfPresent(Action<T> action)
-    {
-        return;
-    }
+        public override void IfPresent(Action<T> action)
+        {
+            return;
+        }
 
-    public override T GetOrElse(T other)
-    {
-        return other;
-    }
+        public override T GetOrElse(T other)
+        {
+            return other;
+        }
 
-    public override bool IsEmpty()
-    {
-        return true;
-    }
+        public override bool IsEmpty()
+        {
+            return true;
+        }
 
-    public override bool IsPresent()
-    {
-        return false;
+        public override bool IsPresent()
+        {
+            return false;
+        }
     }
 }
